4354733e8e3c312cddf43ae07d052b262db05b64
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2023-12-26 17:19:22 +0100

    Faster day 23

--- 2023/23/part2.py
+++ 2023/23/part2.py
@@ -1,7 +1,8 @@
+import collections
 grid = open("input.txt").read().splitlines()
 start, end = (1, 0), (len(grid[0])-2, len(grid)-1)
 addv = lambda a, b: (a[0]+b[0], a[1]+b[1])
-edges = {}
+edges = collections.defaultdict(dict)
 opens = [(start, start, 0, {start})]
 while len(opens):
     pos, startpos, cost, path = opens.pop()
@@ -9,7 +10,8 @@ while len(opens):
     nexts = {None}
     while len(nexts) >= 1:
         if pos == end:
-            edges[tuple(sorted((startpos, pos)))] = edgecost
+            edges[min(startpos, pos)][max(startpos, pos)] = edgecost
+            edges[max(startpos, pos)][min(startpos, pos)] = edgecost
             break
         tile = grid[pos[1]][pos[0]]
         nexts = {addv(pos, d) for d in ((1, 0), (-1, 0), (0, -1), (0, 1))}
@@ -18,25 +20,22 @@ while len(opens):
             cost += 1
             edgecost += 1
             if len(nexts) > 1:
-                key = tuple(sorted((startpos, pos)))
-                if key not in edges:
-                    edges[key] = edgecost
+                if startpos not in edges[pos]:
+                    edges[startpos][pos] = edgecost
+                    edges[pos][startpos] = edgecost
                     for p in nexts:
                         opens.append((p, pos, cost, path.union({p})))
                 break
             else:
                 pos = nexts[0]
                 path.add(pos)
-# Runs in ~3m. There is probably a better algorithm but it's xmas time!
 opens = [(0, start, set())]
 maxcost = 0
 while len(opens) > 0:
     cost, pos, path = opens.pop()
     if pos == end and cost > maxcost:
         maxcost = cost
-    for edge, edgecost in edges.items():
-        if edge[1] == pos:
-            edge = (edge[1], edge[0])
-        if edge[0] == pos and edge[1] not in path:
-            opens.append((cost+edgecost, edge[1], path.union({edge[1]})))
+    for dest, edgecost in edges[pos].items():
+        if dest not in path:
+            opens.append((cost+edgecost, dest, path.union({dest})))
 print(maxcost)
